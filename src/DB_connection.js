0128bf860ab4dca13aae31890ba742b505f088d5
Benjamín Serrano Friedlander <benser22@hotmail.com>
2023-11-05 16:15:42 -0300

    deployment

--- src/DB_connection.js
+++ src/DB_connection.js
@@ -1,14 +1,11 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY } = process.env;
 const FavoriteModel = require("./models/Favorite");
 const UserModel = require("./models/User");
 
-const databaseUrl = process.env.DATABASE_URL;
 
-
-
-const sequelize = new Sequelize(databaseUrl, { logging: false, native: false });
+const sequelize = new Sequelize(DB_DEPLOY, { logging: false, native: false });
 
 
 // const sequelize = new Sequelize(
